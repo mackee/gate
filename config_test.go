78d9e06b31cd28d8088a1af9010e55238f9818f5
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2014-10-14 23:18:58 +0900

    テストを追加

--- config_test.go
+++ config_test.go
@@ -48,6 +48,57 @@ proxy:
 	if conf.Addr != ":9999" {
 		t.Errorf("unexpected address: %s", conf.Addr)
 	}
+
+	if conf.Auth.Header.UserKey != AuthHeaderUserKeyDefault {
+		t.Errorf("unexpected auth header user key: %s", conf.Auth.Header.UserKey)
+	}
+}
+
+func TestParseAuthHeader(t *testing.T) {
+	f, err := ioutil.TempFile("", "")
+	if err != nil {
+		t.Error(err)
+	}
+	defer func() {
+		f.Close()
+		os.Remove(f.Name())
+	}()
+
+	data := `---
+address: ":9999"
+
+auth:
+  session:
+    key: secret
+
+  info:
+    service: 'google'
+    client_id: 'secret client id'
+    client_secret: 'secret client secret'
+    redirect_url: 'http://example.com/oauth2callback'
+
+  header:
+    user_key: 'auth-header-user-key'
+
+htdocs: ./
+
+proxy:
+  - path: /foo
+    dest: http://example.com/bar
+    strip_path: yes
+`
+	if err := ioutil.WriteFile(f.Name(), []byte(data), 0644); err != nil {
+		t.Error(err)
+	}
+
+	conf, err := ParseConf(f.Name())
+	if err != nil {
+		t.Error(err)
+	}
+
+	if conf.Auth.Header.UserKey != "auth-header-user-key" {
+		t.Errorf("unexpected auth header user key: %s", conf.Auth.Header.UserKey)
+	}
 }
 
 func TestParseMultiRestrictions(t *testing.T) {
